15c4ea6f048fca52640bc7b22daf35c9d5dc17e9
Stephen Schulte <sschulte@ucvts.org>
2019-01-15 23:06:06 -0500

    smoother display functionality

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -5,90 +5,106 @@ let displayExpression = ""
 function operations(operation){
     if(operation == '+' && symbol == true){
         expression+='+';
+        displayExpression='';
         document.getElementById("display").innerHTML='0';
         symbol=false;
     }
    
     if(operation == '-' && symbol == true){
         expression+='-';
+        displayExpression='';
         document.getElementById("display").innerHTML='0';
         symbol=false;
     }
 
     if(operation == 'x' && symbol == true){
         expression+='*';
+        displayExpression='';
         document.getElementById("display").innerHTML='0';
         symbol=false;
     }
 
     if(operation == '/' && symbol == true){
         expression+='/';
+        displayExpression='';
         document.getElementById("display").innerHTML='0';
         symbol=false;
     }
 
     if(operation == '.' && symbol == true){
         expression+='.';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='.';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=false;
     }
 }
 
 function numbers(number){
     if(number == '0'){
-        expression+="0";
-        document.getElementById("display").innerHTML=expression;
+        expression+='0';
+        displayExpression+='0';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '1'){
         expression+='1';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='1';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '2'){
         expression+='2';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='2';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '3'){
         expression+='3';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='3';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '4'){
         expression+='4';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='4';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '5'){
         expression+='5';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='5';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '6'){
         expression+='6';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='6';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '7'){
         expression+='7';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='7';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '8'){
         expression+='8';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='8';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
     if(number == '9'){
         expression+='9';
-        document.getElementById("display").innerHTML=expression;
+        displayExpression+='9';
+        document.getElementById("display").innerHTML=displayExpression;
         symbol=true;
     }
 } 
 
 function erase(){
     expression='';
+    displayExpression=''
     document.getElementById("display").innerHTML='0';
     symbol=true;
 }
@@ -100,12 +116,14 @@ function equals(input){
             document.getElementById("display").innerHTML="Error!";
         }
         else if(evaluation>=1000000000){
-            let exponent=finalExpression.toString().length-1;
-            document.getElementById("display").innerHTML=evaluation/(10**(exponent))+'e'+exponent;
+            let exponent=evaluation.toString().length-1;
+            displayExpression=evaluation/(10**(exponent))+'e'+exponent;
+            document.getElementById("display").innerHTML=displayExpression;
         }
         else{
             document.getElementById("display").innerHTML=evaluation.toLocaleString();
         }        
+
     }
 }
 
